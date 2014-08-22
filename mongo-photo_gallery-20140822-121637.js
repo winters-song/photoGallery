ecbff7be6718e13b74bc32427668e8db92a022aa
winters-song <songyang@outlook.com>
2014-08-22 18:20:47 +0800

    test

---
+++ mongo-photo_gallery-20140822-121637.js
@@ -0,0 +1,255 @@
+
+/** photos indexes **/
+db.getCollection("photos").ensureIndex({
+  "_id": NumberInt(1)
+},[
+  
+]);
+
+/** users indexes **/
+db.getCollection("users").ensureIndex({
+  "_id": NumberInt(1)
+},[
+  
+]);
+
+/** photos records **/
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408605758784,
+  "update_time": 1408605758784,
+  "file": "/files/47cf2226a3145d8c2aff3615ebecde60.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\47cf2226a3145d8c2aff3615ebecde60.jpg",
+  "thumb": "/files/thumb/47cf2226a3145d8c2aff3615ebecde60.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\47cf2226a3145d8c2aff3615ebecde60.jpg",
+  "width": NumberInt(1920),
+  "height": NumberInt(1200),
+  "_id": ObjectId("53f59e3e8a54f73016487934")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408606205040,
+  "update_time": 1408606205040,
+  "file": "/files/66865a21b02d74f671e7c03ea9d04048.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\66865a21b02d74f671e7c03ea9d04048.jpg",
+  "thumb": "/files/thumb/66865a21b02d74f671e7c03ea9d04048.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\66865a21b02d74f671e7c03ea9d04048.jpg",
+  "width": NumberInt(1920),
+  "height": NumberInt(1200),
+  "_id": ObjectId("53f59ffd8a54f73016487935")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408614498461,
+  "update_time": 1408614498461,
+  "file": "/files/9436f73d801bf34719ce90222de330f5.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\9436f73d801bf34719ce90222de330f5.jpg",
+  "thumb": "/files/thumb/9436f73d801bf34719ce90222de330f5.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\9436f73d801bf34719ce90222de330f5.jpg",
+  "width": NumberInt(1920),
+  "height": NumberInt(1080),
+  "_id": ObjectId("53f5c062dfec5d6424103bdb")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691745708,
+  "update_time": 1408691745708,
+  "file": "/files/9e7b358a89654502d0fc359da9c47e3b.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\9e7b358a89654502d0fc359da9c47e3b.jpg",
+  "thumb": "/files/thumb/9e7b358a89654502d0fc359da9c47e3b.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\9e7b358a89654502d0fc359da9c47e3b.jpg",
+  "width": NumberInt(1920),
+  "height": NumberInt(1080),
+  "_id": ObjectId("53f6ee211afa2984147b19de")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691752497,
+  "update_time": 1408691752498,
+  "file": "/files/4d2b1cbc32dc1f3fe3bcf52cb1ad2af6.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\4d2b1cbc32dc1f3fe3bcf52cb1ad2af6.jpg",
+  "thumb": "/files/thumb/4d2b1cbc32dc1f3fe3bcf52cb1ad2af6.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\4d2b1cbc32dc1f3fe3bcf52cb1ad2af6.jpg",
+  "width": NumberInt(1440),
+  "height": NumberInt(900),
+  "_id": ObjectId("53f6ee281afa2984147b19df")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691760044,
+  "update_time": 1408691760044,
+  "file": "/files/311544d59adf499680e25e8830d0d5ec.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\311544d59adf499680e25e8830d0d5ec.jpg",
+  "thumb": "/files/thumb/311544d59adf499680e25e8830d0d5ec.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\311544d59adf499680e25e8830d0d5ec.jpg",
+  "width": NumberInt(2560),
+  "height": NumberInt(1600),
+  "_id": ObjectId("53f6ee301afa2984147b19e0")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691773394,
+  "update_time": 1408691773394,
+  "file": "/files/f2ed52dfc667795b89314b391471ce1a.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\f2ed52dfc667795b89314b391471ce1a.jpg",
+  "thumb": "/files/thumb/f2ed52dfc667795b89314b391471ce1a.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\f2ed52dfc667795b89314b391471ce1a.jpg",
+  "width": NumberInt(1920),
+  "height": NumberInt(1200),
+  "_id": ObjectId("53f6ee3d1afa2984147b19e1")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691781665,
+  "update_time": 1408691781665,
+  "file": "/files/3959a5e9b1a32594f51755e36ce938fc.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\3959a5e9b1a32594f51755e36ce938fc.jpg",
+  "thumb": "/files/thumb/3959a5e9b1a32594f51755e36ce938fc.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\3959a5e9b1a32594f51755e36ce938fc.jpg",
+  "width": NumberInt(1920),
+  "height": NumberInt(1440),
+  "_id": ObjectId("53f6ee451afa2984147b19e2")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691789573,
+  "update_time": 1408691789573,
+  "file": "/files/f8e69146b1653ded0593ccea4833e141.png",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\f8e69146b1653ded0593ccea4833e141.png",
+  "thumb": "/files/thumb/f8e69146b1653ded0593ccea4833e141.png",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\f8e69146b1653ded0593ccea4833e141.png",
+  "width": NumberInt(1680),
+  "height": NumberInt(1050),
+  "_id": ObjectId("53f6ee4d1afa2984147b19e3")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691799187,
+  "update_time": 1408691799187,
+  "file": "/files/de8169dd4cff4b5fc4255fb6d4d69591.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\de8169dd4cff4b5fc4255fb6d4d69591.jpg",
+  "thumb": "/files/thumb/de8169dd4cff4b5fc4255fb6d4d69591.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\de8169dd4cff4b5fc4255fb6d4d69591.jpg",
+  "width": NumberInt(2560),
+  "height": NumberInt(1440),
+  "_id": ObjectId("53f6ee571afa2984147b19e4")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691806004,
+  "update_time": 1408691806004,
+  "file": "/files/200f77c102911e93f5b9acecbc94b2f9.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\200f77c102911e93f5b9acecbc94b2f9.jpg",
+  "thumb": "/files/thumb/200f77c102911e93f5b9acecbc94b2f9.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\200f77c102911e93f5b9acecbc94b2f9.jpg",
+  "width": NumberInt(2560),
+  "height": NumberInt(1600),
+  "_id": ObjectId("53f6ee5e1afa2984147b19e5")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691813904,
+  "update_time": 1408691813904,
+  "file": "/files/cd1627e3eb56b30d4f4dd9a04351e3a7.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\cd1627e3eb56b30d4f4dd9a04351e3a7.jpg",
+  "thumb": "/files/thumb/cd1627e3eb56b30d4f4dd9a04351e3a7.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\cd1627e3eb56b30d4f4dd9a04351e3a7.jpg",
+  "width": NumberInt(1680),
+  "height": NumberInt(1050),
+  "_id": ObjectId("53f6ee651afa2984147b19e6")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691893907,
+  "update_time": 1408691893907,
+  "file": "/files/3e20829068af4a3307ff7f6327c4e7a0.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\3e20829068af4a3307ff7f6327c4e7a0.jpg",
+  "thumb": "/files/thumb/3e20829068af4a3307ff7f6327c4e7a0.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\3e20829068af4a3307ff7f6327c4e7a0.jpg",
+  "width": NumberInt(2560),
+  "height": NumberInt(1600),
+  "_id": ObjectId("53f6eeb51afa2984147b19e7")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691905522,
+  "update_time": 1408691905522,
+  "file": "/files/625ec76e86bd847ab06a3609b4c46d46.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\625ec76e86bd847ab06a3609b4c46d46.jpg",
+  "thumb": "/files/thumb/625ec76e86bd847ab06a3609b4c46d46.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\625ec76e86bd847ab06a3609b4c46d46.jpg",
+  "width": NumberInt(2560),
+  "height": NumberInt(1600),
+  "_id": ObjectId("53f6eec11afa2984147b19e8")
+});
+db.getCollection("photos").insert({
+  "aid": "",
+  "tags": null,
+  "description": null,
+  "upload_time": 1408691918306,
+  "update_time": 1408691918306,
+  "file": "/files/8dee65d3db32f833279f71b7d901b5d1.jpg",
+  "file_path": "e:\\git\\photoGallery\\public\\files\\8dee65d3db32f833279f71b7d901b5d1.jpg",
+  "thumb": "/files/thumb/8dee65d3db32f833279f71b7d901b5d1.jpg",
+  "thumb_path": "e:\\git\\photoGallery\\public\\files\\thumb\\8dee65d3db32f833279f71b7d901b5d1.jpg",
+  "width": NumberInt(2560),
+  "height": NumberInt(1600),
+  "_id": ObjectId("53f6eece1afa2984147b19e9")
+});
+
+/** system.indexes records **/
+db.getCollection("system.indexes").insert({
+  "v": NumberInt(1),
+  "key": {
+    "_id": NumberInt(1)
+  },
+  "ns": "photo_gallery.users",
+  "name": "_id_"
+});
+db.getCollection("system.indexes").insert({
+  "v": NumberInt(1),
+  "key": {
+    "_id": NumberInt(1)
+  },
+  "ns": "photo_gallery.photos",
+  "name": "_id_"
+});
+
+/** users records **/
+db.getCollection("users").insert({
+  "name": "admin",
+  "password": "4297f44b13955235245b2497399d7a93",
+  "root": true,
+  "creatable": true,
+  "editable": true,
+  "removable": true,
+  "_id": ObjectId("53ec85ae67df925420c40ed4")
+});
